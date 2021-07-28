1491dce055dee4ae8c62300d8f99a248421a3765
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2021-07-28 14:49:53 +0200

    Initial commit

---
+++ src/main/kotlin/io/mateusz/linksharer/LinkSharerApplication.kt
@@ -0,0 +1,11 @@
+package io.mateusz.linksharer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LinkSharerApplication
+
+fun main(args: Array<String>) {
+    runApplication<LinkSharerApplication>(*args)
+}
---
+++ src/test/kotlin/io/mateusz/linksharer/LinkSharerApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.mateusz.linksharer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LinkSharerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
