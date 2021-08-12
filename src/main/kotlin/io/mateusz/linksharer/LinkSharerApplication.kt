e4c15ea76f1de82802b21a166e264389107edfa7
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2021-08-12 14:57:02 +0200

    Add swagger documentation

--- src/main/kotlin/io/mateusz/linksharer/LinkSharerApplication.kt
+++ src/main/kotlin/io/mateusz/linksharer/LinkSharerApplication.kt
@@ -2,9 +2,28 @@ package io.mateusz.linksharer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import springfox.documentation.builders.PathSelectors
 
+import springfox.documentation.builders.RequestHandlerSelectors
+
+import springfox.documentation.spi.DocumentationType
+
+import springfox.documentation.spring.web.plugins.Docket
+
+@EnableWebMvc
 @SpringBootApplication
-class LinkSharerApplication
+class LinkSharerApplication {
+    @Bean
+    fun api(): Docket? {
+        return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<LinkSharerApplication>(*args)
